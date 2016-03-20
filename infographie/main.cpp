5caf0d5eb6a5a8c90cbeddc3b57d5d834e8ffa80
Honion <jeffrey.honion@gmail.com>
2016-03-20 23:26:48 +0100

    Camera + viewport 1.0

--- infographie/main.cpp
+++ infographie/main.cpp
@@ -13,6 +13,13 @@ using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const int rendererwidth  = 1000;
+const int rendererheight = 1000;
+const int texturewidth  = 1024;
+const int textureheight = 1024;
+const int camera_z = 5;
+const Vec3f eye(1,1,3);
+const Vec3f center(0,0,0);
 
 //Algorithme de Brensenham
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -45,6 +52,69 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
             error -= dx*2;
         }
     }
+}
+
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
+}
+
+Matrix v2m(float x1, float x2, float x3) {
+    Matrix m(4, 1);
+    m[0][0] = x1;
+    m[1][0] = x2;
+    m[2][0] = x3;
+    m[3][0] = 1.f;
+    return m;
+}
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = 255/2.f;
+
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = 255/2.f;
+    return m;
+}
+
+Matrix lookat(Vec3f eye, Vec3f center, Vec3f up) {
+    Vec3f z = (eye-center).normalize();
+    Vec3f x = (up^z).normalize();
+    Vec3f y = (z^x).normalize();
+    Matrix res = Matrix::identity(4);
+    for (int i=0; i<3; i++) {
+        res[0][i] = x[i];
+        res[1][i] = y[i];
+        res[2][i] = z[i];
+        res[i][3] = -center[i];
+    }
+    return res;
+}
+
+void affichermatrix(Matrix m) {
+
+  for (int i=0; i<m.ncols(); i++) {
+    for (int j=0; j<m.nrows(); j++) {
+      std::cout << m[i][j] << "\t";
+    }
+    std::cout << std::endl;
+  }
+  cout<<endl;
+}
+Matrix Multimatrix(Matrix m1,Matrix m2) {
+   Matrix res = Matrix(m1.nrows(), m2.ncols());
+
+    for (unsigned int i = 0; i < m1.nrows(); i++) {
+      for (unsigned int j = 0; j < m2.ncols(); j++) {
+        float val = 0;
+        for (unsigned int k = 0; k < m1.ncols(); k++) {
+          val += m1[i][k] * m2[k][j] ;
+        }
+        res[i][j]= val;
+      }
+    }
+return res;
 }
 
 void load_texture(std::string filename, const char *suffix, TGAImage &img) {
@@ -83,6 +153,8 @@ float* barycentre(float x1, float y1, float x2, float y2, float x3, float y3, in
 
 
 TGAImage dessin(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float xt1, float yt1, float xt2, float yt2, float xt3, float yt3, float intensity, float * zbuffer, TGAImage image, TGAImage diffusemap){
+
+    //cout<< x1 << " x1"<< y1 << " y1"<< z1 << " z1" << endl;
     int minx = std::min(std::min(x1 , x2), x3);
     int miny = std::min(std::min(y1 , y2 ), y3);
     int maxx = std::max(std::max(x1 , x2), x3);
@@ -93,16 +165,17 @@ TGAImage dessin(float x1, float y1, float z1, float x2, float y2, float z2, floa
     maxx = std::min(image.get_width()-1, maxx);
     maxy = std::min(image.get_height()-1, maxy);
 
+    //cout<< minx << "minimim" << endl ;
     for (int i=minx; i<=maxx; i++){
         for (int j=miny; j<=maxy; j++){
             float* bar = new float[3];
             bar = barycentre(x1, y1, x2, y2, x3, y3, i, j);
-            if ( (((x1-i)*(y2-j)-(y1-j)*(x2-i))>=0 && ((x2-i)*(y3-j)-(y2-j)*(x3-i))>=0 && ((x3-i)*(y1-j)-(y3-j)*(x1-i))>=0 ) || (((x1-i)*(y2-j)-(y1-j)*(x2-i))<=0 && ((x2-i)*(y3-j)-(y2-j)*(x3-i))<=0 && ((x3-i)*(y1-j)-(y3-j)*(x1-i))<=0 ) ) {
+            if ( bar[0]>=0 && bar[1]>=0 && bar[2]>=0 ) {
                 float z = bar[0]*z1 + bar[1]*z2 + bar[2]*z3;
-                if ( z > zbuffer[i+j*1000] ) {
-                    zbuffer[i+j*1000] = z;
-                    int x = (bar[0]*xt1 + bar[1]*xt2 + bar[2]*xt3) * 1024;
-                    int y = (bar[0]*yt1 + bar[1]*yt2 + bar[2]*yt3) * 1024;
+               if ( z > zbuffer[i+j*rendererheight] ) {
+                    zbuffer[i+j*rendererheight] = z;
+                    int x = (bar[0]*xt1 + bar[1]*xt2 + bar[2]*xt3) * diffusemap.get_width();
+                    int y = (bar[0]*yt1 + bar[1]*yt2 + bar[2]*yt3) * diffusemap.get_height();
                     image.set(i, j, TGAColor(diffusemap.get(x,y).r*intensity, diffusemap.get(x,y).g*intensity, diffusemap.get(x,y).b*intensity,255));
                 }
             }
@@ -179,9 +252,12 @@ double getprofondeur(string v) {
 
 // Dessine les triangles
 void creation_triangle(vector<string> tabv, vector<string> tabf, vector<string> tabvt ,  vector<string> tabft,  TGAImage diffusemap) {
-	TGAImage image(1000, 1000, TGAImage::RGB);
+	TGAImage image(rendererheight, rendererwidth, TGAImage::RGB);
 	int point1 = -1, point2 = -1, point3 = -1, pointv1 = -1, pointv2 = -1, pointv3 = -1;
-	float* zbuffer = new float[1000*1000];
+	float* zbuffer = new float[rendererheight*rendererwidth];
+
+
+
 	for (int i = 0; i < tabf.size(); i++) {
 		string ligne = tabf[i];
 		char * pch;
@@ -243,9 +319,9 @@ void creation_triangle(vector<string> tabv, vector<string> tabf, vector<string> 
 
 
 
-		x0 = (getabscisse(v1) + 1) * 500;
-		x1 = (getabscisse(v2) + 1) * 500;
-		x2 = (getabscisse(v3) + 1) * 500;
+		x0 = (getabscisse(v1) + 1) ;
+		x1 = (getabscisse(v2) + 1);
+		x2 = (getabscisse(v3) + 1);
 
 		xt0 = getabscisse(vt1);
 		xt1 = getabscisse(vt2);
@@ -253,18 +329,18 @@ void creation_triangle(vector<string> tabv, vector<string> tabf, vector<string> 
 
 
 
-		y0 = (getordonnee(v1) + 1) * 500;
-		y1 = (getordonnee(v2) + 1) * 500;
-		y2 = (getordonnee(v3) + 1) * 500;
+		y0 = (getordonnee(v1) + 1) ;
+		y1 = (getordonnee(v2) + 1) ;
+		y2 = (getordonnee(v3) + 1) ;
 
 		yt0 = getordonnee(vt1);
 		yt1 = getordonnee(vt2);
 		yt2 = getordonnee(vt3);
 
 
-        z0 = (getprofondeur(v1) + 1) * 500;
-        z1 = (getprofondeur(v2) + 1) * 500;
-        z2 = (getprofondeur(v3) + 1) * 500;
+        z0 = (getprofondeur(v1) + 1) ;
+        z1 = (getprofondeur(v2) + 1) ;
+        z2 = (getprofondeur(v3) + 1) ;
 
         Vec3f world_coords[3];
         world_coords[0] = { x0,y0,z0 };
@@ -272,20 +348,45 @@ void creation_triangle(vector<string> tabv, vector<string> tabf, vector<string> 
         world_coords[2] = { x2,y2,z2 };
 
 
+
         Vec3f light_dir(0,0,-1);
+
+        Matrix Projection = Matrix::identity(4);
+        Matrix ViewPort = viewport(0,0,  image.get_height()*2/4,   image.get_width()*2/4);
+        Projection[3][2] = -1.f/camera_z;
+
+        Vec3i screen_coords[3];
+        Matrix m(4,4);
+        Matrix ModelView = lookat(eye, center, Vec3f(0,1,0));
+        Matrix resultat;
+        resultat = Multimatrix(ViewPort,Projection);
+        Matrix resultat1;
+        resultat1 = Multimatrix(resultat, ModelView);
+        m = v2m(x0,y0,z0);
+        m=Multimatrix(resultat1,m);
+        screen_coords[0] = { m2v(m)};
+        m = v2m(x1,y1,z1);
+        m=Multimatrix(resultat1,m);
+        screen_coords[1] = { m2v(m)};
+        m = v2m(x2,y2,z2);
+        m=Multimatrix(resultat1,m);
+        screen_coords[2] = { m2v(m)};
         Vec3f n = (world_coords[2] - world_coords[0])^(world_coords[1]-world_coords[0]);
         n.normalize();
         float intensity = n * light_dir;
 
-        if(intensity > 0){
-             image = dessin(x0,y0,z0,x1,y1,z1,x2,y2,z2,xt0,yt0,xt1,yt1,xt2,yt2,intensity, zbuffer, image, diffusemap);
+    if(intensity > 0){
+            image = dessin(screen_coords[0].x,screen_coords[0].y,screen_coords[0].z,screen_coords[1].x,screen_coords[1].y,screen_coords[1].z,screen_coords[2].x,screen_coords[2].y,screen_coords[2].z,
+                            xt0,yt0,xt1,yt1,xt2,yt2,intensity, zbuffer, image, diffusemap);
         }
 	}
+
 	image.flip_vertically();
 	image.write_tga_file("visage.tga");
+	delete[] zbuffer;
 }
 
-// Lire le fichier objet ( f et v )
+// Lire le fichier objet ( f , v  et vt)
 int lecture_objet(const std::string & path) {
 	fstream fichier(path.c_str());
 
@@ -382,7 +483,6 @@ int lecture_objet(const std::string & path) {
 
 
 int main(int argc, char** argv) {
-    TGAImage image(1000, 1000, TGAImage::RGB);
 
     lecture_objet("./african_head.obj");
     return 0;
